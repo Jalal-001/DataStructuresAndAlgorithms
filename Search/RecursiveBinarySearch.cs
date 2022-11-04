90f2053a7737fdeaaed22abed5a3ab8a9e9a63ec
Jalal-001 <xaliqlicelal661@gmail.com>
2022-11-04 09:51:07 +0400

    binary search & recursive b.s

---
+++ Search/RecursiveBinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    public class RecursiveBinarySearch
+    {
+        // generic 
+        public static int Search<T>(T[] array, int first, int last, T key)
+            where T : IComparable
+        {
+            int middle = (last + first) / 2;
+            if (array[middle].Equals(key))
+                return middle;
+            else if (last > first)
+                return -1;
+            else if (key.CompareTo(array[middle]) < 1) // key<array[middle]
+                return Search(array, first, middle, key);
+            else
+                return Search(array, middle + 1, last, key);
+        }
+
+
+        // non-generic
+        //public static int Search(int[]array,int first,int last,int key)
+        //{
+        //    int middle = (first + last) / 2;
+        //    if (array[middle] == key)
+        //        return middle;
+        //    else if (array[middle] > key)
+        //        return Search(array, first, middle, key);
+        //    else 
+        //        return Search(array, middle + 1, last, key);
+        //}
+    }
+}
---
+++ SearchApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Search;
+
+var arr = new int[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+//var result = BinarySearch.Search(arr,80);
+var result = RecursiveBinarySearch.Search(arr, 0, arr.Length - 1, 40);
+
+
+if (result != -1)
+    Console.WriteLine($"arr[{result}]={arr[result]}");
+else
+    Console.WriteLine("Searched item not found");
+
