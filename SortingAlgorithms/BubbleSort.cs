e136de1545393e0544074ab88ebaf6a1ac7abfd3
Jalal-001 <xaliqlicelal661@gmail.com>
2022-11-07 10:36:30 +0400

    sorting algorithms

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,19 @@
+﻿namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        public static void Sort<T>(T[] array)
+            where T : IComparable<T>
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int o = 0; o < n - i - 1; o++) // n-i-1 => remove past number and continue
+                {
+                    if (array[o].CompareTo(array[o + 1]) >= 1) // [0]>[o+1]
+                        Sorting.Swap(array, o, o + 1);
+                }
+            }
+        }
+    }
+}
---
+++ SortingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using SortingAlgorithms;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var array = new int[] { 10, 30, 50, 45, 60, 90, 80 };
+		Write(array);
+		SortingAlgorithms.BubbleSort.Sort(array);
+        Write(array);
+
+        static void Write<T>(T[] array)
+		{
+			Console.WriteLine();
+			foreach (var item in array)
+			{
+				Console.Write($"{item,-7}");
+			}
+            Console.WriteLine();
+        }
+    }
+}
+
